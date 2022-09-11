69f56f0075d7a93c329ebaa50e6566187a46caa7
Elmer Mendieta <emendieta60236@ufide.ac.cr>
2022-09-11 14:26:49 -0600

    implementing the script

--- app/js/index.js
+++ app/js/index.js
@@ -1,7 +1,15 @@
 const questions = document.querySelectorAll(".container-questions");
 
 questions.forEach((question) => {
-  question.addEventListener("click", (e) => {
+  const button = question.querySelector(".question img");
+
+  button.addEventListener("click", () => {
+    questions.forEach((item) => {
+      if (item !== question) {
+        item.classList.remove("open");
+      }
+    });
+
     question.classList.toggle("open");
   });
 });
